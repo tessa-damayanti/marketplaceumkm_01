3dc8798c3d05df692fc915e84e489e7b57b77223
Ezraa27 <ezrameritaringoringo@gmail.com>
2026-04-07 12:12:54 +0700

    buat tampilan login, home page, dan download tailwind

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,6 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
     ],
     server: {
         watch: {
